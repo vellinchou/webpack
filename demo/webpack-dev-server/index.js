2d6a5f6c93aee3fb92e8f477d6d93767c6f40960
vellinChou <esther_chou@163.com>
2017-02-20 22:09:14 +0800

    Update index.js

--- demo/webpack-dev-server/index.js
+++ demo/webpack-dev-server/index.js
@@ -2,7 +2,7 @@ var webpack = require( 'webpack' );
 var WebpackDevServer = require( 'webpack-dev-server' );
 
 var config = require( "./webpack.config.js" );
-config.entry.app.unshift( "webpack-dev-server/client?http://localhost:8080/", "webpack/hot/dev-server" );
+config.entry.app.unshift( "webpack-dev-server/client?http://localhost:9001/", "webpack/hot/dev-server" );
 
 var compiler = webpack( config );
 
